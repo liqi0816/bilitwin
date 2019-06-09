41cb5b47d375b43c2a319a3639b15c2223097f02
Xmader <xmader@outlook.com>
2019-06-09 00:46:38 -0400

    gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -27,60 +27,6 @@ const replace = require('gulp-replace');
 const babelCore = require("babel-core");
 const jsxac = require('jsx-append-child');
 
-gulp.task('default', ['build']);
-gulp.task('build', ['biliTwin.user.js', 'biliTwinBabelCompiled.user.js']);
-
-gulp.task('biliTwin.user.js', ['./src/bilitwin.js'], () => {
-    if (!includeSourcemap) {
-        return gulp.src(['./src/bilitwin.meta.js', './src/bilitwin.js'])
-            .pipe(sourcemaps.init({ loadMaps: true }))
-            .pipe(concat('biliTwin.user.js'))
-            .pipe(gulp.dest('.'));
-    }
-    else {
-        return gulp.src(['./src/bilitwin.meta.js', './src/bilitwin.js'])
-            .pipe(sourcemaps.init({ loadMaps: true }))
-            .pipe(concat('biliTwin.user.js'))
-            .pipe(sourcemaps.write('.', {
-                sourceMappingURLPrefix: 'https://raw.githubusercontent.com/liqi0816/bilitwin/develop'
-            }))
-            .pipe(gulp.dest('.'));
-    }
-});
-
-gulp.task('biliTwinBabelCompiled.user.js', ['biliTwin.user.js'], async () => new Promise((resolve, reject) => {
-    babelCore.transformFile('biliTwin.user.js', {
-        presets: ['env'],
-        plugins: [["babel-plugin-transform-builtin-extend", { globals: ["DataView"] }]]
-    }, (err, { code }) => {
-        if (err) return reject(err);
-        fs.readFile('./src/bilitwin.meta.js', (err, file) => {
-            if (err) return reject(err);
-            meta = file.toString();
-            gulp.src(['./src/bilitwin-babel.entry.js'])
-                .pipe(replace('"${metadata}"', meta))
-                .pipe(replace(/(\/\/[ ]?@name\s+)(?=\w)/, '$1(Babel)'))
-                .pipe(replace(/(\/\/[ ]?@description\s+)(?=\w)/, '$1(国产浏览器和Edge浏览器专用)'))
-                .pipe(replace('"${transpiledCode}"', code))
-                .pipe(rename('biliTwinBabelCompiled.user.js'))
-                .pipe(gulp.dest('.'))
-                .on('end', resolve)
-                .on('error', reject);
-        })
-    });
-}));
-
-gulp.task('./src/bilitwin.js', ['./src/ui/ui.js', './src/flvass2mkv/interface.js'], async () => {
-    const bundle = await rollup.rollup({
-        input: './src/bilitwin.entry.js'
-    });
-    return bundle.write({
-        file: './src/bilitwin.js',
-        format: 'es',
-        sourcemap: includeSourcemap,
-    })
-});
-
 gulp.task('./src/ui/ui.js', () => {
     return gulp.src('./src/ui/ui.entry.js')
         .pipe(babel({
@@ -104,6 +50,57 @@ gulp.task('./src/flvass2mkv/interface.js', async () => new Promise((resolve, rej
         }
     })
 }));
+
+gulp.task('./src/bilitwin.js', gulp.series(gulp.parallel('./src/ui/ui.js', './src/flvass2mkv/interface.js'), async () => {
+    const bundle = await rollup.rollup({
+        input: './src/bilitwin.entry.js'
+    });
+    return bundle.write({
+        file: './src/bilitwin.js',
+        format: 'es',
+        sourcemap: includeSourcemap,
+    })
+}));
+
+gulp.task('biliTwin.user.js', gulp.series('./src/bilitwin.js', () => {
+    if (!includeSourcemap) {
+        return gulp.src(['./src/bilitwin.meta.js', './src/bilitwin.js'])
+            .pipe(sourcemaps.init({ loadMaps: true }))
+            .pipe(concat('biliTwin.user.js'))
+            .pipe(gulp.dest('.'));
+    }
+    else {
+        return gulp.src(['./src/bilitwin.meta.js', './src/bilitwin.js'])
+            .pipe(sourcemaps.init({ loadMaps: true }))
+            .pipe(concat('biliTwin.user.js'))
+            .pipe(sourcemaps.write('.', {
+                sourceMappingURLPrefix: 'https://raw.githubusercontent.com/liqi0816/bilitwin/develop'
+            }))
+            .pipe(gulp.dest('.'));
+    }
+}));
+
+gulp.task('biliTwinBabelCompiled.user.js', gulp.series('biliTwin.user.js', async () => new Promise((resolve, reject) => {
+    babelCore.transformFile('biliTwin.user.js', {
+        presets: ['env'],
+        plugins: [["babel-plugin-transform-builtin-extend", { globals: ["DataView"] }]]
+    }, (err, { code }) => {
+        if (err) return reject(err);
+        fs.readFile('./src/bilitwin.meta.js', (err, file) => {
+            if (err) return reject(err);
+            meta = file.toString();
+            gulp.src(['./src/bilitwin-babel.entry.js'])
+                .pipe(replace('"${metadata}"', meta))
+                .pipe(replace(/(\/\/[ ]?@name\s+)(?=\w)/, '$1(Babel)'))
+                .pipe(replace(/(\/\/[ ]?@description\s+)(?=\w)/, '$1(国产浏览器和Edge浏览器专用)'))
+                .pipe(replace('"${transpiledCode}"', code))
+                .pipe(rename('biliTwinBabelCompiled.user.js'))
+                .pipe(gulp.dest('.'))
+                .on('end', resolve)
+                .on('error', reject);
+        })
+    });
+})));
 
 gulp.task('bump-minor-version', async () => {
     await new Promise((resolve, reject) => {
@@ -146,10 +143,13 @@ gulp.task('bump-patch-version', async () => {
     });
 });
 
-gulp.task('release-minor', ['bump-minor-version', 'build'])
+gulp.task('build', gulp.series('biliTwinBabelCompiled.user.js'));
+gulp.task('default', gulp.series('build'));
 
-gulp.task('release-patch', ['bump-patch-version', 'build'])
+gulp.task('release-minor', gulp.series('bump-minor-version', 'build'))
+
+gulp.task('release-patch', gulp.series('bump-patch-version', 'build'))
 
 gulp.task('watch', () => {
-    return gulp.watch(['./src/ui/ui.entry.js'], { delay: 5000 }, ['./src/ui/ui.js']);
+    return gulp.watch(['./src/ui/ui.entry.js'], { delay: 5000 }, gulp.series('./src/ui/ui.js'));
 });
