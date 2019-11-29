1f8c47de9fdd00ef9e408bce36ad3355e173c614
Xmader <xmader@outlook.com>
2019-11-28 23:36:03 -0500

    TextDecoder polyfill

--- src/bilitwin-babel.entry.js
+++ src/bilitwin-babel.entry.js
@@ -26,6 +26,22 @@ function load() {
         }
     }
 
+    if (typeof TextDecoder === 'undefined') {
+        top.TextDecoder = function () {
+            this.encoding = 'utf-8';
+            this.decode = function (input) {
+                if (input instanceof ArrayBuffer) {
+                    input = new Uint8Array(input);
+                } else {
+                    input = new Uint8Array(input.buffer);
+                }
+
+                var l = Array.prototype.map.call(input, function (x) { return String.fromCharCode(x) }).join("");
+                return decodeURIComponent(escape(l));
+            };
+        }
+    }
+
     if (typeof _babelPolyfill === 'undefined') {
         new Promise(function (resolve) {
             var req = new XMLHttpRequest();
